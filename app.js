baad82cde2dc26c185e6d846f4f23f643a374a25
Vagaweb <fabien.casters@epitech.eu>
2014-07-03 10:04:25 +0200

    It's better like this. :)

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
-var Blog = require('./lib/blog')
+var express = require('express')
+  , Blog = require('./lib/blog')
 
-var blog = Blog(require('./config'));
-// Let's gooo !
+var app = express();
+var config = require('./config');
+
+app.set('views', './views');
+app.set('view engine', 'jade');
+
+var blog = Blog(app, config);
 blog.run();
+
+// Let's gooo !
+app.listen(8106); // 8106 = BLOG :P
