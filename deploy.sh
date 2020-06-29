47475b3c5dbdcc15de72433320c33ce32b9dd1cc
Fabien CASTERS <fabien@vaga.io>
2020-06-29 19:50:20 +0200

    :lipstick: Update theme

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To Public folder
-cd public
+pushd public
 # Add changes to git.
 git add .
 
@@ -21,4 +21,4 @@ git commit -m "$msg"
 git push origin master
 
 # Come Back up to the Project Root
-cd ..
+popd
